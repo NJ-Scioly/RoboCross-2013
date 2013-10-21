7361b2c0d1ad1930147cb0b6b4f34c190bb494e9
NJ-Scioly <NJ-Scioly@users.noreply.github.com>
2013-10-21 19:23:29 -0400

    Update ScienceOlympiadRoboCross.ino

--- ScienceOlympiadRoboCross.ino
+++ ScienceOlympiadRoboCross.ino
@@ -1,4 +1,9 @@
 /*
+
+This projects license (the following MIT license) does NOT apply for anyone entering the competition. However, the software is still provided "as is" (see line 21).
+If you are participating in the 2013 Science Olympiad RoboCross event, you may NOT under any circumstances copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software.
+================================================================================
 The MIT License (MIT)
 
 Copyright (c) 2013 Neil Johari
@@ -19,6 +24,8 @@ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 */
 #include <Servo.h>
 
