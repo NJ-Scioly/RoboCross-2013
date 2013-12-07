d98f536b3046566512c14b9d58b4a8b485379a5d
skipperguy12 <neiljohari@gmail.com>
2013-12-07 16:49:14 -0500

    Add another servo (5th) for plow control.
    Pressing button C allows plow to go upwards (counter clock wise), and Z allows plow to drop (clock wise).

--- RoboCross_2013/RoboCross_2013.ino
+++ RoboCross_2013/RoboCross_2013.ino
@@ -16,15 +16,13 @@ const int MC = 20;
 // But: For a car like this, to move a back wheel forward, you need to send it the backwards command
 // And forward to go backwards!
 
-
-
 // The leeway zone
 const int leeway = 2;
 
 // Constant joystick positions
-const int analogCenterX = 124;
-const int analogCenterY = 122;
-const int analogLeft = 23;
+const int analogCenterX = 133;
+const int analogCenterY = 126;
+const int analogLeft = 34;
 const int analogRight = 227;
 const int analogUp = 220;
 const int analogDown = 27;
@@ -33,10 +31,10 @@ const int analogDown = 27;
 ArduinoNunchuk nunchuk = ArduinoNunchuk();
 
 // Array of servos
-Servo servos[4];
+Servo servos[5];
 
-// Array of pins the servos are attached to, index: {0=>Front Right, 1=>Back Right, 2=>Front Left, 3=>Back Left}
-int pins[] = {3, 10, 9, 11};
+// Array of pins the servos are attached to, index: {0=>Front Right, 1=>Back Right, 2=>Front Left, 3=>Back Left, 4=>plow}
+int pins[] = {3, 10, 9, 11, 5};
 
 void setup()
 {
@@ -53,10 +51,11 @@ void setup()
 #define FL servos[1] // Back right
 #define BR servos[2] // Front left
 #define BL servos[3] // Back left
+#define PLOW servos[4] // Plow
 
 void setupServos(){
   // Loop through the servos and attach a pin to it
-  for(int i = 0 ; i < 4 ; i++){
+  for(int i = 0 ; i < 5 ; i++){
     Serial.print("Attaching servo");
     Serial.print(' ');
     Serial.print(i, DEC);
@@ -76,6 +75,10 @@ void stopServos(){
   BR.write(MS);
   FL.write(MS);
   BL.write(MS);
+} 
+
+void stopPlow(){
+ PLOW.write(MS); 
 }
 
 void loop()
@@ -83,6 +86,21 @@ void loop()
 
   // Update the controller
   nunchuk.update();
+  
+  // Begin Plow
+  
+  if(nunchuk.cButton == 1){
+    // C Button pressed!
+     PLOW.write(MCC);
+  } else if (nunchuk.zButton == 1){
+   // ZButton is pressed!
+    PLOW.write(MC); 
+  } else {
+   stopPlow(); 
+  }
+  
+  
+  // End plow
   
   // If the joystick is in a position where it will not be affected, stop them all
   if((nunchuk.analogX < nunchuk.analogX + leeway || nunchuk.analogY < nunchuk.analogY + leeway)&&(nunchuk.analogX > nunchuk.analogX - leeway || nunchuk.analogY > nunchuk.analogY - leeway)){
